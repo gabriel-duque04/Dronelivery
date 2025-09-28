e1ff71a0c814b08d6f42f8e955c8a1e1f2b49237
Gabriel Fernandes Duque dos Santos <gabrielduquefernandes@gmail.com>
2025-09-28 13:43:43 -0300

    feat: Adiciona estrutura inicial do backend e models

---
+++ backend/DroneliveryService/Models/Drone.cs
@@ -0,0 +1,37 @@
+﻿namespace DroneliveryService.Models
+{
+    public class Drone
+    {
+        private int _id;
+        private double _capacidadeMaximaKg;
+        private double _autonomiaMaximaKm;
+        private double _bateriaAtual;
+
+
+        //Getters e Setters da classe:
+        public int Id
+        {
+            get { return _id; }
+            private set { _id = value; }
+        }
+
+        public double CapacidadeMaximaKg
+        {
+            get { return _capacidadeMaximaKg; }
+            private set { _capacidadeMaximaKg = value; }
+        }
+
+        public double AutonomiaMaximaKm
+        {
+            get { return _autonomiaMaximaKm; }
+            private set { _autonomiaMaximaKm = value; }
+        }
+
+        public double BateriaAtual
+        {
+            get { return _bateriaAtual; }
+            set { _bateriaAtual = value; } // A bateria pode ser alterada externamente por enquanto
+        }
+
+    }
+}
---
+++ backend/DroneliveryService/Models/Pedido.cs
@@ -0,0 +1,51 @@
+﻿namespace DroneliveryService.Models
+{
+    public class Pedido
+    {
+        private int _id;
+        private int _localizacaoX;
+        private int _localizacaoY;
+        private double _pesoPacote;
+        private int _prioridade;
+        private DateTime _dataHoraPedido;
+
+
+
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int LocalizacaoX
+        {
+            get { return _localizacaoX; }
+            set { _localizacaoX = value; }
+        }
+
+        public int LocalizacaoY
+        {
+            get { return _localizacaoY; }
+            set { _localizacaoY = value; }
+        }
+
+        public double PesoPacote
+        {
+            get { return _pesoPacote; }
+            set { _pesoPacote = value; }
+        }
+
+        public int Prioridade
+        {
+            get { return _prioridade; }
+            set { _prioridade = value; }
+        }
+
+        public DateTime DataHoraPedido
+        {
+            get { return _dataHoraPedido; }
+            set { _dataHoraPedido = value; }
+        }
+    }
+}
